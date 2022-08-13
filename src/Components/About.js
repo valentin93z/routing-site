e8c79f019157d358403ab9044f40950a8c068c87
valentin93z <valentin.zagrebun.93@gmail.com>
2022-08-13 15:11:17 +0300

    complete About section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,32 @@
 import React from "react";
 import '../Styles/About.css';
+import { useDispatch, useSelector } from "react-redux";
+import { openQuestionsHowItWorks, openQuestionsSafeDriving } from '../features/scooterSlice';
 
 // import img
 import arr3 from '../img/arr3.svg';
 import arr4 from '../img/arr4.svg';
+import arrTop from '../img/arr-top.svg';
+import arrBottom from '../img/arr-bottom.svg';
+
+
 
 function About() {
+
+    const dispatch = useDispatch();
+    const howItWorks = useSelector((state) => state.scooter.howItWorks);
+    const safeDriving = useSelector((state) => state.scooter.safeDriving);
+
+
+    const howItWorksHandler = (id) => {
+        dispatch(openQuestionsHowItWorks(id));
+    }
+
+    const safeDrivingHandler = (id) => {
+        dispatch(openQuestionsSafeDriving(id));
+    }
+
+
     return (
         <div className="About">
             <div className="about-cover">
@@ -41,6 +62,71 @@ function About() {
                         <h2>Better urban living</h2>
                         <p>We’re helping connect cities and bring people closer together. Our scooters are also fully-electric and we offset the minimal carbon footprint for each ride.</p>
                     </div>
+                </div>
+            </div>
+            <div className="about-sb">
+                <h2>Our values</h2>
+                <div className="about-sb-cards">
+                    <div className="about-sb-card">
+                        <div className="about-sb-card-img-1">
+                            <div className="about-sb-orange-circle">01</div>
+                        </div>
+                        <h4>Our tech</h4>
+                        <p>We’re using cutting edge technology to drive accessible urban transportation forward. Our fully electric scooters are a joy to ride!</p>
+                    </div>
+                    <div className="about-sb-card">
+                        <div className="about-sb-card-img-2">
+                            <div className="about-sb-orange-circle">02</div>
+                        </div>
+                        <h4>Our integrity</h4>
+                        <p>We are fully committed to deliver a great yet safe, sustainable micro-mobility experience in every city we serve.</p>
+                    </div>
+                    <div className="about-sb-card">
+                        <div className="about-sb-card-img-3">
+                            <div className="about-sb-orange-circle">03</div>
+                        </div>
+                        <h4>Our community</h4>
+                        <p>We support every community we serve. All workers are paid a living wage based on their location and are Scoot employees.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="faq-block">
+                <h2>FAQs</h2>
+                <div className="howItWorks">
+                    <h3>How it works</h3>
+                    <ul>
+                        {
+                            howItWorks.map((q) => {
+                                return (
+                                    <li key={q.id} onClick={(id) => howItWorksHandler(q.id)}>
+                                        <div className="question-head">
+                                            <h4>{q.question}</h4>
+                                            <img alt="" src={q.status ? arrTop : arrBottom}/>
+                                        </div>
+                                        <p className={q.status ? 'answerDisplay' : 'answerNone'}>{q.answer}</p>
+                                    </li>
+                                )
+                            })
+                        }
+                     </ul>
+                 </div>
+                 <div className="safeDriving">
+                     <h3>Safe driving</h3>
+                     <ul>
+                        {
+                            safeDriving.map((q) => {
+                                return (
+                                    <li key={q.id} onClick={(id) => safeDrivingHandler(q.id)}>
+                                        <div className="question-head">
+                                            <h4>{q.question}</h4>
+                                            <img alt="" src={q.status ? arrTop : arrBottom}/>
+                                        </div>
+                                        <p className={q.status ? 'answerDisplay' : 'answerNone'}>{q.answer}</p>
+                                    </li>
+                                )
+                            })
+                        }
+                     </ul>
                 </div>
             </div>
         </div>
--- src/features/scooterSlice.js
+++ src/features/scooterSlice.js
@@ -2,6 +2,46 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     burgerOpen: false,
+    howItWorks: [
+        {
+            question: 'How do I download the app?',
+            answer: 'To download the Scoot app, you can search “Scoot” in both the App and Google Play stores. An even simpler way to do it would be to click the relevant link at the bottom of this page and you’ll be re-directed to the correct page.',
+            status: false,
+            id: 'q1',
+        },
+        {
+            question: 'Can I find a nearby Scoots?',
+            answer: 'Yes, you can find a nearby Scoots!',
+            status: false,
+            id: 'q2',
+        },
+        {
+            question: 'Do I need a license to ride?',
+            answer: 'Yes, you need a license to ride!',
+            status: false,
+            id: 'q3',
+        },
+    ],
+    safeDriving: [
+        {
+            question: 'Should I wear a helmet?',
+            answer: 'Yes, please do! All cities have different laws. But we strongly strongly strongly recommend always wearing a helmet regardless of the local laws. We like you and we want you to be as safe as possible while Scooting.',
+            status: false,
+            id: 'q1',
+        },
+        {
+            question: 'How about the rules & regulations?',
+            answer: 'Some text',
+            status: false,
+            id: 'q2',
+        },
+        {
+            question: 'What if I damage my Scoot?',
+            answer: 'Some text',
+            status: false,
+            id: 'q3',
+        },
+    ],
 }
 
 export const scooterSlice = createSlice({
@@ -17,9 +57,17 @@ export const scooterSlice = createSlice({
             state.burgerOpen = false;
             const body = document.getElementById("body");
             body.classList.remove('stopScroll');
+        },
+        openQuestionsHowItWorks: (state, action) => {
+            const toggleStatus = state.howItWorks.find((q) => q.id === action.payload);
+            toggleStatus.status = !toggleStatus.status;
+        },
+        openQuestionsSafeDriving: (state, action) => {
+            const toggleStatus = state.safeDriving.find((q) => q.id === action.payload);
+            toggleStatus.status = !toggleStatus.status;
         }
     }
 })
 
-export const { toggleBurger, autoCloseMenu } = scooterSlice.actions;
+export const { toggleBurger, autoCloseMenu, openQuestionsHowItWorks, openQuestionsSafeDriving } = scooterSlice.actions;
 export default scooterSlice.reducer;
